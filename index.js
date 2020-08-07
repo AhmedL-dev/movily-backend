b29461ec23333e66cbb8ac927407e03ae0c5809b
AhmedL-dev <laadraoui.ahmed@gmail.com>
2020-08-07 15:50:49 +0100

    init

--- index.js
+++ index.js
@@ -12,5 +12,4 @@ require('./startup/prod')(app)
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => winston.info(`Listening on port ${port}...`))
-
 module.exports = server
--- startup/db.js
+++ startup/db.js
@@ -4,6 +4,8 @@ const config = require('config')
 
 module.exports = function() {
   const db = config.get('db')
+  // const db = 'mongodb+srv://vidlyuser:1234@vidly.tbi22.azure.mongodb.net/vidly?retryWrites=true&w=majority'
+  console.log(db)
   mongoose.connect(db)
     .then(() => winston.info(`Connected to ${db}...`))
 }
