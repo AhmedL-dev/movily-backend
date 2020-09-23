dfea8482db702b0b67a4ab44fc5a9ffcb6e7b006
AhmedL-dev <laadraoui.ahmed@gmail.com>
2020-09-23 21:37:08 +0100

    update users

--- routes/users.js
+++ routes/users.js
@@ -1,47 +1,50 @@
-const auth = require('../middleware/auth')
-const _ = require('lodash')
-const bcrypt = require('bcrypt')
-const {User,validate} = require('../models/user')
-const mongoose = require('mongoose')
-const express = require('express')
-const router = express.Router()
+const auth = require("../middleware/auth");
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
+const { User, validate } = require("../models/user");
+const mongoose = require("mongoose");
+const express = require("express");
+const router = express.Router();
 
+router.get("/me", auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password");
+  res.send(user);
+});
 
-router.get('/me', auth, async (req, res) => {
-  const user = await User.findById(req.user._id).select('-password')
-  res.send(user)
-})
+router.get("/", async (req, res) => {
+  const users = await User.find().sort("name");
+  res.send(users);
+});
 
-router.get('/', async (req, res) => { 
-  const users = await User.find().sort('name')
-  res.send(users)
-})
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-router.post('/', async (req, res) => {
-  const {error} = validate(req.body)
-  if (error) return res.status(400).send(error.details[0].message)
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send("User already registered.");
 
-  let user = await User.findOne({ email: req.body.email })
-  if (user) return res.status(400).send('User already registered.')
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(req.body.password, salt);
 
-  user = new User(_.pick(req.body, ['name', 'email', 'password']))
-  const salt = await bcrypt.genSalt(10)
-  user.password = await bcrypt.hash(req.body.password, salt)
+  await user.save();
 
-  await user.save()
+  const token = user.generateAuthToken();
+  res
+    .header("x-auth-token", token)
+    .send(_.pick(user, ["_id", "name", "email"]));
+});
 
-  const token = user.generateAuthToken()
-  res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']))
-})
+router.put("/:id", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-router.put('/:id', async (req, res) => {
-  const {error} = validate(req.body)
-  if (error) return res.status(400).send(error.details[0].message)
+  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  if (!user) return res.status(400).send("Invalide User Id");
 
-  const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true})
-  if (!user) return res.status(400).send('Invalide User Id')
+  res.send(user);
+});
 
-  res.send(user)
-})
-
-module.exports = router
+module.exports = router;
